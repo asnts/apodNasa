d086bdf9d88ef1432bef79e8d597535fdcbd554e
Annanda Santos <annandasants@gmail.com>
2022-02-03 18:25:14 -0300

    ajustes no css e exibição da imagem de hoje

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
+$.ajax({
+    url:`https://api.nasa.gov/planetary/apod?api_key=tG7LS6QnaFH8fcSsXGcVM2UYe13iP3TpvybAy6kr`, 
+
+success: function(response){
+            
+    $(".resultado").css("display", "block");
+    $("#video").css("display", "none")
+    $(".titulo").text(`${response.title}`); // titulo da imagem
+    $(".titulo").css ("color", "whitesmoke");    
+    $("#data").text(`${response.date}`); // data selecionada
+
+    if (response.media_type == "image"){
+        $(".imgdia").css ('background-image', `url(${response.url})`);
+        $(".imgdia").css ("display", "block");
+    } else {
+        $(".imgdia").css("display", "none")
+        $("#video").css("display", "block")
+        $("#video").attr("src", response.url);
+
+    }
+    $(".descricao").text(`${response.explanation}`); // Descrição da imagem-dia
+    $(".descricao").css("display", "block");
+
+},
+
+error: function (){
+    $(".resultado").css("display", "flex");
+    $("titulo").text(`API não encontrada`);
+    $("descricao").text(`Data não encontrada: Por favor, selecione uma data entre 20 de junho de 1995 e a data de hoje`);
+                
+}
+})
 
 
-  $('#btn').click(function(event){   
+
+
+
+$('#btn').click(function(event){   
       event.preventDefault()   
     const data = $("#data").val(); 
     
@@ -26,7 +61,7 @@
                 $("#video").attr("src", response.url);
 
             }
-            $(".descricao").text(`${response.explanation}`); //Data section imagem-dia
+            $(".descricao").text(`${response.explanation}`); //Descrição da imagem-dia
             $(".descricao").css("display", "block");
 
         },
